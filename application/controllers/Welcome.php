c9f1ee8ff45f7bda6b47edf0373c9ba7368232d7
taufik <taufikridho@gmail.com>
2019-05-30 01:10:03 +0700

    Membuat controller Pendapatan desa

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -35,7 +35,44 @@ class Welcome extends MY_Controller {
 				->set_js('../bower_components/morrisjs/morris.min', TRUE)
 				->set_js('morris-data', TRUE)
 				->build('welcome/welcome_message');
-		//$this->template->build('welcome/welcome_message');
+	
+            
+                $this->load->model('pendapatan/M_pendapatan');
+                $data2 = $this->M_pendapatan->get_data()->result();
+              //  var_dump($data)or die();
+                $x['data'] = json_encode($data2);
+                $this->load->view('welcome/welcome_message',$x);
+      
+//            $this->load->model('pendapatan/M_pendapatan');
+////		$this->load->model('auth/user_model');
+//		$data['pendapatan'] = $this->inventaris_model->get_data()->result();
+//		$data['get_all_data'] = $this->inventaris_model->get_all_data();
+//		$data['get_day_data'] = $this->inventaris_model->get_day_data();
+//		$data['jumlah_user'] = $this->user_model->get_all_data();
+                
+              
+   
+        //  $this->load->view('welcome/welcome_message',$y);
+                
+              // $data[] = array();
+		$data_kategori = array();
+                $data_realisasi = array();
+                
+                foreach ($this->M_pendapatan->getChartData_Angkatan()->result_array() as $row){
+                    
+                   
+			$data[] = (string)$row['desa'];
+			$data_kategori[] = (int) $row['anggaran'];
+                        $data_realisasi[] = (int) $row['realisasi'];
+		}
+                
+                
+                $this->load->view('welcome/welcome_message', array(
+			'data'=>$data, 
+			'data_kategori'=>$data_kategori
+                        ,'data_realisasi'=>$data_realisasi)
+		);
+               //var_dump($data[])or die();
 	}
 
 }
